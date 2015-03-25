725ef8919858b547bc1375b35a49eb0c191dd712
juliangommans <apprentice@StMac6.local>
2015-03-25 17:49:45 +1300

    added testing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    redirect_to new_user_registration_path unless user_signed_in?
+    #redirect_to new_user_registration_path unless user_signed_in?
     @products = Product.all
   end
 
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, :type => :controller do
 
+  describe '#index' do
+
+    before do
+      get :index
+    end
+
+    it 'returns an http status 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders an index page' do
+      expect(response).to render_template('index')
+    end
+
+    it 'loads all products' do
+      product1, product2 = Product.create!, Product.create!
+      get :index
+      expect(assigns(:products)).to match_array([product1, product2])
+    end
+
+  end
+
+
+
+
+
 end
